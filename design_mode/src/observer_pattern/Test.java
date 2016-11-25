baee2c269a12ef7622cc58d191bb60ba936a956c
handler <925616096@qq.com>
2016-11-25 17:54:30 +0800

    修改了readme.txt

--- design_mode/src/observer_pattern/Test.java
+++ design_mode/src/observer_pattern/Test.java
@@ -1,6 +1,11 @@
 package observer_pattern;
 
-//观察者模式：定义了对象之间的一对多依赖，这样一来，当一个对象改变状态时，它的所有依赖者多会收到通知并自动更新
+/**
+ * 观察者模式：定义了对象之间的一对多依赖，这样一来，当一个对象改变状态时，它的所有依赖者多会收到通知并自动更新
+ * 为了交互对象之间的松耦合设计二努力
+ * @author zhangpeng
+ *
+ */
 public class Test {
 
 	public static void main(String[] args) {
