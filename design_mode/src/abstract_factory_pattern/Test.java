262ba2620fd06ee3516f6583fbce8f0597cd8265
handler <925616096@qq.com>
2016-12-06 14:02:31 +0800

    抽象工厂模式

---
+++ design_mode/src/abstract_factory_pattern/Test.java
@@ -0,0 +1,14 @@
+package abstract_factory_pattern;
+
+public class Test {
+
+	/**
+	 * 抽象工厂模式提供一个接口，用于创建相关或依赖对象的家族，而不需要明确指定具体类
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
